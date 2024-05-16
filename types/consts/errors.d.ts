e3d85fc907dec1846ed7a4d2e9967ee963871adf
latha_d_p <latha@riversilica.com>
2024-05-16 14:20:31 +0530

    16

---
+++ types/consts/errors.d.ts
@@ -0,0 +1,18 @@
+declare namespace _default {
+    const NetworkBadStatus: string;
+    const NetworkRequestFailed: string;
+    const NetworkRequestAborted: string;
+    const NetworkRequestTimeout: string;
+    const NetworkBodyParserFailed: string;
+    const StreamingHlsPlaylistParserError: string;
+    const StreamingDashManifestParserError: string;
+    const StreamingContentSteeringParserError: string;
+    const StreamingVttParserError: string;
+    const StreamingFailedToSelectNextSegment: string;
+    const StreamingFailedToDecryptSegment: string;
+    const StreamingFailedToTransmuxSegment: string;
+    const StreamingFailedToAppendSegment: string;
+    const StreamingCodecsChangeError: string;
+}
+export default _default;
+//# sourceMappingURL=errors.d.ts.map
